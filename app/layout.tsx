c359b3d4cab949e6a59ed9f95aedc3e87f5e54b1
abdelbasset4 <benatmani1999@gmail.com>
2023-11-05 17:51:23 +0100

    finish registeration new user

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,31 @@
-"use client"
-import './globals.css'
-import { Poppins } from 'next/font/google'
-import ThemeProvider from './utils/ThemeProvider'
-import { Toaster } from 'react-hot-toast'
-import {Provider} from "react-redux"
-import store from "../Redux/Store"
+"use client";
+import "./globals.css";
+import { Poppins } from "next/font/google";
+import ThemeProvider from "./utils/ThemeProvider";
+import { Toaster } from "react-hot-toast";
+import { ReduxProvider } from "./Provider";
 const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ["400","500","600", "700"],
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
   variable: "--font-family-poppins",
-})
+});
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${poppins.variable} !bg-white dark:bg-primary dark:from-grey dark:to-primary duration-300`}>
-        <Provider store={store}>
-        <ThemeProvider attribute='class' defaultTheme='system' enableSystem>
-          {children}
-          <Toaster position='top-center' reverseOrder={false}/>
-        </ThemeProvider>
-        </Provider>
+      <body
+        className={`${poppins.variable} !bg-white dark:bg-primary dark:from-grey dark:to-primary duration-300`}>
+        <ReduxProvider>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            {children}
+            <Toaster position="top-center" reverseOrder={false} />
+          </ThemeProvider>
+        </ReduxProvider>
       </body>
     </html>
-  )
+  );
 }
